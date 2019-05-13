58c758e882db91dee6f220255e0b725edabe4e7a
Mina Naguib <mina@naguib.ca>
2019-05-13 16:34:44 -0400

    Output CSV for easier machining of reports

--- bench.c
+++ bench.c
@@ -1,4 +1,5 @@
 #include <time.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <openssl/sha.h>
@@ -6,22 +7,32 @@
 
 int main(int argc, char ** argv) {
 	char input[MAXLEN+1] = {0};
+	double rate;
+	int interactive = isatty(fileno(stdout));
 
 	setvbuf(stdout, NULL, _IONBF, 0);
 
 	for (int len=1; len <= MAXLEN; len++) {
 		input[len-1] = '0' + (len % 10);
 
-		printf("Benchmarking input length %3d: ", len);
+		if (interactive) {
+			printf("Benchmarking input length %3d: ", len);
+		}
 		int x=clock();
 		benchmark(input, len);
 		x = clock() - x;
-		printf(
-				"%d reps in %d ticks (%f/s)\n",
-				LOOPS,
-				x,
-				(double)LOOPS / ((double)x/CLOCKS_PER_SEC)
-		      );
+		rate = (double)LOOPS / ((double)x/CLOCKS_PER_SEC);
+		if (interactive) {
+			printf(
+					"%d reps in %d ticks (%.f/s)\n",
+					LOOPS,
+					x,
+					rate
+			      );
+		}
+		else {
+			printf("%d,%.f\n", len, rate);
+		}
 
 	}
 	return 0;
